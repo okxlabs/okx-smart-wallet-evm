195bec0222262d4ecf8a3aafaff95b86c5a5a388
backdoor <20609724+doutv@users.noreply.github.com>
2025-08-22 17:58:59 +0800

    Update WebAuthn import paths for consistency in PasskeyValidatorLib and test files

--- src/libraries/PasskeyValidatorLib.sol
+++ src/libraries/PasskeyValidatorLib.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.29;
 
 import {P256} from "@openzeppelin/contracts/utils/cryptography/P256.sol";
 import {MerkleProofProcessor} from "./MerkleProofProcessor.sol";
-import {WebAuthn} from "webauthn-sol/WebAuthn.sol";
+import {WebAuthn} from "webauthn-sol/src/WebAuthn.sol";
 
 /**
  * @title PasskeyValidatorLib
