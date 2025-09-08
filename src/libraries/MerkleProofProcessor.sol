ca19dffaf5a7e1e18b729fcf77104b544cf637fa
Kurtney <75674208+KurtneySh@users.noreply.github.com>
2025-09-08 17:59:46 +0800

    chore: relicense src folder from GPL-3.0 to MIT (#105)

--- src/libraries/MerkleProofProcessor.sol
+++ src/libraries/MerkleProofProcessor.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
