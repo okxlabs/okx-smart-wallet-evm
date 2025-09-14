5c3a15fb565b124e2b59fe14702f6814f298ea0a
jingkang.chua <jingkang.chua@okg.com>
2025-09-14 16:12:51 +0800

    test: remove unused ISmartWalletSimulator import from Simulation tests
    - Cleaned up import statement that was no longer needed
    - All 10 tests still passing after cleanup

--- test/Simulation.t.sol
+++ test/Simulation.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.23;
 import {Base, MockERC20} from "./Base.t.sol";
 import {Call, BatchedCall} from "src/Types.sol";
 import {ISmartWallet} from "src/interfaces/ISmartWallet.sol";
-import {ISmartWalletSimulator} from "script/utils/ISmartWalletSimulator.s.sol";
 import {console} from "forge-std/console.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC712} from "src/ERC712.sol";
