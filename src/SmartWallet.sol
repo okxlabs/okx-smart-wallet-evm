a6876f9279c0d71d0ee1bd48f855f027065f2217
fumeng00mike <141021510+fumeng00mike@users.noreply.github.com>
2025-09-14 12:16:38 +0800

    fixed: executeUserOp only support v0.7.0 (#123)
    Co-authored-by: mengmeng.fu <mengmeng.fu@okg.com>

--- src/SmartWallet.sol
+++ src/SmartWallet.sol
@@ -102,7 +102,7 @@ abstract contract SmartWallet is
     /// @dev This function is executable only by the EntryPoint contract, and is the main pathway for UserOperations to be executed.
     /// UserOperations can be executed through the execute function, but another method of authorization (ie through a passed in signature) is required.
     /// userOp.callData is abi.encodePacked(IAccountExecute.executeUserOp.selector, (abi.encode(Call[]))
-    /// Note that this contract is only compatible with Entrypoint versions v0.7.0 and v0.8.0. It is not compatible with v0.6.0, as that version does not support the "executeUserOp" selector.
+    /// Note that this contract is only compatible with Entrypoint versions v0.7.0.
     function executeUserOp(
         PackedUserOperation calldata userOp,
         bytes32
